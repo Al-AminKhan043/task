ddca80665820b9dffc70ea43f9bd0a9f8b1d4047
Al Amin <alaminkhan10009@gmail.com>
2025-02-27 21:53:11 +0600

    ok

--- p/src/App.jsx
+++ p/src/App.jsx
@@ -1,19 +1,18 @@
-import './App.css';  // Keep this for additional custom styling if necessary.
+import './App.css';
 import Payment from './components/Payment';
 import Guide from './components/Guide';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from './components/HomePage';
+
 function App() {
   return (
-    <>
-      <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/payment" element={<Payment />} />
         <Route path="/guide" element={<Guide />} />
       </Routes>
     </Router>
-    </>
   );
 }
 
--- p/vite.config.js
+++ p/vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+
+const isProduction = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss(),
-  ],
-  base: '/task/', 
-})
+  plugins: [react(), tailwindcss()],
+  base: isProduction ? '/task/' : '/',
+});
